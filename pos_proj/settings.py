c40d618394f36cdc8fc7177e45e78f70d5d23276
Manoj Sarna <newsindigital@gmail.com>
2021-08-05 22:22:11 +0530

    settings updated

--- pos_proj/settings.py
+++ pos_proj/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'pos_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+# STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
